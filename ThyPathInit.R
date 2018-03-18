f476d0bd77b936f876c351a6c72abe6666214661
JH.LITMUS <litmus@JH-MacBook-Pro.local>
2018-03-19 01:50:57 +0900

    convert for ThyPath

--- ThyPathInit.R
+++ ThyPathInit.R
@@ -1,34 +1,21 @@
-dat <- data.frame(var=c("Histologic type", "Tumor margin",
-                        "Maximum diameter of invasive tumor",
-                        "Maximum diameter of invasive and in situ carcinoma",
-                        "Nuclear grade",
-                        "Histologic grade", 
-                        "Lymphovascular invasion",
-                        "Perineural invasion",
-                        "Intraductal carcinoma component", 
-                        "Proportion",
-                        "Type",
-                        "Nuclear grade", 
-                        "Comedo type necrosis",
-                        "Location",
-                        "Calcification",
-                        "Associated benign lesion",
-                        "Nipple and areola",
-                        "Paget's disease",
-                        "Lactiferous duct involvement",
-                        "Dermal lymphatic invasion",
-                        "Resection margins",
-                        "Overlying skin",
-                        "Underlying fascia",
-                        "node",
-                        "ER",
-                        "PR",
-                        "HER2",
-                        "p53",
-                        "Ki"), val='NA')
+dat <- data.frame(var=c("ID",
+                        "Pathology",
+                        "Histologic subtype",
+                        "Maximum diameter of tumor",
+                        "Tumor involvement",
+                        "central",
+                        "ret-central",
+                        "mediastinal",
+                        "ret-mediastinal",
+                        "lateral",
+                        "ret-lateral",
+                        "total",
+                        "ret-total",
+                        "max size",
+                        "perinodal"), val='NA',stringsAsFactors = FALSE)
 
 data.all <- dat
 
-data.all <- read.csv("PathparseRaw.csv", header=TRUE, sep=",",check.names=FALSE )
+data.all <- read.csv("PathParseRaw.csv", header=TRUE, sep=",",check.names=FALSE )
 
 data.all[,272] <- NULL
